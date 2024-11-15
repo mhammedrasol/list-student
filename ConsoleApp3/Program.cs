6e5986c638af7c33efa2303155b9a698179ab931
mhammedrasol <83mbamad@gmail.com>
2024-11-15 16:40:13 +0300

    q2

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,45 +9,13 @@ namespace Liststudent
 {
     internal class Program
     {
-        class Student
-        {
-            public string Name { get; set; }
-            public int Marks { get; set; }
-
-            public Student(string name, int marks)
-            {
-                Name = name;
-                Marks = marks;
-            }
-        }
+        
+         
+       
         static void Main(string[] args)
         {
-            List<Student> students = new List<Student>();
-
-          
-
-            for (int i = 0; i < 2; i++)
-            {
-                Console.WriteLine("\nEnter details for Student :"+i);
-
-                Console.Write("Name: ");
-                string name = Console.ReadLine();
-
-
-                Console.Write("Marks: ");
-                int marks = int.Parse(Console.ReadLine());
-
-                students.Add(new Student(name, marks));
-            }
-
-            foreach (var student in students)
-            {
-                if (student.Marks >= 50)
-                {
-                    Console.WriteLine("THE STUDENT IS PASS:"+student.Name);
-                }
-            }
-            Console.ReadLine();
+            Console.WriteLine("q2");
+            Console.Read();
         }
     }
 }
