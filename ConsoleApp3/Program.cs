5e6ea76be9db63bb632ae0b1f7c01f922f2da0d9
mhammedrasol <83mbamad@gmail.com>
2024-11-15 16:31:10 +0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liststudent
+{
+    internal class Program
+    {
+        class Student
+        {
+            public string Name { get; set; }
+            public int Marks { get; set; }
+
+            public Student(string name, int marks)
+            {
+                Name = name;
+                Marks = marks;
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+          
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("\nEnter details for Student :"+i);
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+
+                Console.Write("Marks: ");
+                int marks = int.Parse(Console.ReadLine());
+
+                students.Add(new Student(name, marks));
+            }
+
+            foreach (var student in students)
+            {
+                if (student.Marks >= 50)
+                {
+                    Console.WriteLine("THE STUDENT IS PASS:"+student.Name);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
